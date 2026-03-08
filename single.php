861c0614366bad10278f1e4a49ceb991ac2ca711
Basem <basem@wewebit.com>
2026-03-08 22:24:18 +0300

    add-edit-date

--- single.php
+++ single.php
@@ -23,10 +23,12 @@
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                             <span><?php echo esc_html(get_the_date('j F Y')); ?></span>
                         </div>
+                        <p class="text-slate-500">آخر تحديث: <?php echo esc_html(get_the_modified_date('j F Y')); ?></p>
                         <div class="flex items-center gap-2">
                             <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                             <span><?php echo esc_html(toc_estimated_reading_time()); ?></span>
                         </div>
+
                     </div>
                 </div>
 
